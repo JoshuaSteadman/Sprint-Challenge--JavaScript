cbb0530d76eb3db6922bd76eed5c88b127816c75
Joshua Steadman <sintal1ty@gmail.com>
2019-08-23 11:18:28 -0400

    function.js done

--- challenges/functions.js
+++ challenges/functions.js
@@ -1,11 +1,14 @@
 // ==== Callbacks ====  
 
-/* Step 1: Create a higher-order function
-  * Create a higher-order function named consume with 3 parameters: a, b and cb
-  * The first two parameters can take any argument (we can pass any value as argument)
-  * The last parameter accepts a callback
-  * The consume function should return the invocation of cb, passing a and b into cb as arguments
+/* Step 1: Create a higher-order function that accepts a callback
+  * Create a higher-order function named consume that can take 3 parameters.
+  * The first two parameters can accept any argument
+  * The last parameter accepts a callback 
+  * In the body of the function return the callback with the two parameters that you created
 */
+function consume(a,b,cb){
+  return cb(a,b);
+}
 
 
 /* Step 2: Create several functions to callback with consume();
@@ -13,19 +16,30 @@
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add(a,b){
+  return a+b;
+}
+
+function multiply(a,b){
+  return a*b;
+}
+function greeting(first_name,last_name){
+  return `Hello ${first_name} ${last_name}, nice to meet you!`;
+}
+
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 
-// Explain in your own words why nestedfunction can access the variable internal.
+// Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation: 
+// Explanation: JavaScript compiler allows nested functions to have access to all parameters and variables above it will continue to look to up for a reference.
 
 
 const external = "I'm outside the function";
